ffff9ef114a3aa1781a2e99c1bb1038b8d9987c9
Pat Holland <pholls@users.noreply.github.com>
2021-04-09 16:06:05 -0700

    milestone 2 updates

--- Assets/SpriteDetector.cs
+++ Assets/SpriteDetector.cs
@@ -6,6 +6,7 @@ public class SpriteDetector : MonoBehaviour
 {
     private float pressing = 0;
     public Animator animator;
+
     void Start()
     {
         addPhysics2DRaycaster();
@@ -27,32 +28,13 @@ public class SpriteDetector : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
 
-            //animator.SetBool("Press", true);
             pressing = 1.0f;
-            Debug.Log("pressing");
         }
         else
         {
-            //animator.SetBool("Press", false);
             pressing = 0.0f;
-            //Debug.Log("not pressing");
         }
 
         animator.SetFloat("Pressing", pressing);
     }
-    //public void OnMouseDown()
-    //{
-    //    Debug.Log("Mouse Down");
-    //    // switch chest sprite to "compressed"
-    //    animator.SetBool("Press", true);
-    //    // adjust HealthBarTimer appropriately
-    //}
-
-    //public void OnMouseUp()
-    //{
-    //    Debug.Log("Mouse Up");
-
-    //    // switch chest sprite to "regular"
-    //    animator.SetBool("Press", false);
-    //}
 }
